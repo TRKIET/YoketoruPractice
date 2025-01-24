f9e004fed84612141bacb4171d809aa3e6d30d63
trkiet <167387891+TRKIET@users.noreply.github.com>
2025-01-24 15:40:52 +0900

    1

---
+++ Assets/Yoketoru/Scripts/Game/InputController.cs
@@ -0,0 +1,22 @@
+ using UnityEngine;
+
+
+public class InputController
+{
+   
+     IInput[] inputs = {
+
+     };
+ 
+   
+     public void Update()
+     {
+  
+     }
+ 
+
+     public Vector2 GetValue()
+     {
+          return Vector2.zero;
+         }
+}
